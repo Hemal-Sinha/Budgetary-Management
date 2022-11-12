8139156a42a59dea1fc0b6d467a4dde4342f5545
Hemal-Sinha <92432757+Hemal-Sinha@users.noreply.github.com>
2022-11-13 03:16:10 +0530

    Update complete.php

--- complete.php
+++ complete.php
@@ -9,7 +9,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Details</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="Styling/bud.css">
@@ -19,7 +19,7 @@ session_start();
 <body>
 
 
-<body style="background-image: url('Images/bgimg.jpg');background-size: cover;">
+<body style="background-image: url('Images/background.webp');background-size: cover;">
 	<nav class="navbar navbar-expand-lg sticky-top bg-primary">
     	<div class="container-fluid">
       		<span class="navbar-brand" href="#">
@@ -40,7 +40,7 @@ session_start();
       		</div>
       		<span class="navbar-brand" href="#">
         		<?php echo "Hello, ".$_SESSION['User']."!"?>
-				<a href="first.php">
+				<a href="logout.php">
 				<img src="Images/logout.png" width="70" height="40">
 				</a>
       		</span>
