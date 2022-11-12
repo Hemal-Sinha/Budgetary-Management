7100df2af2102a84a10717f851d387b37bcf195a
Hemal-Sinha <92432757+Hemal-Sinha@users.noreply.github.com>
2022-11-13 03:22:22 +0530

    Update home_page.php

--- home_page.php
+++ home_page.php
@@ -9,10 +9,10 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Home Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="Styling/home.css">
+    <!-- <link rel="stylesheet" href="Styling/style.css"> -->
     <!-- <script src="first.js"></script> -->
 </head>
 
@@ -38,7 +38,7 @@ session_start();
       </div>
       <span class="navbar-brand" href="#">
         <?php echo "Hello, ".$_SESSION['User']."!"?>
-        <a href="first.php">
+        <a href="logout.php">
           <img src="Images/logout.png" width="70" height="40">
         </a>
       </span>
@@ -49,19 +49,22 @@ session_start();
   <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
   <div class="carousel-inner">
   <div class="carousel-item active">
+      <img src="Images/management.jpg" class="w-100" style="height:500px;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Images/budget rule.png" class="w-100" style="height:500px;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Images/types.jpg" class="w-100" style="height:500px;" alt="...">
+    </div>
+    <div class="carousel-item">
       <img src="Images/st.png" class="w-100" style="height:500px;" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="Images/1.png" class="w-100" style="height:500px;" alt="...">
+      <img src="Images/saving.webp" class="w-100" style="height:500px;" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="Images/2.jpeg" class="w-100" style="height:500px;" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="Images/3.jpeg" class="w-100" style="height:500px;" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="Images/4.jpeg" class="w-100" style="height:500px;" alt="...">
+      <img src="Images/third.jpg" class="w-100" style="height:500px;" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
